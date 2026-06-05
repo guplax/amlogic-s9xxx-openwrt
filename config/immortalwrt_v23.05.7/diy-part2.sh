54817b3ac7c6451a9daae081c3d1f772de69c04c
Guplax <198092412+guplax@users.noreply.github.com>
2026-06-05 01:45:06 +0000

    Refactor the code

--- config/immortalwrt_v23.05.7/diy-part2.sh
+++ config/immortalwrt_v23.05.7/diy-part2.sh
@@ -9,16 +9,16 @@
 # ------------------------------- Main source configuration -------------------------------
 #
 # Set the default LAN IP address
-default_ip="192.168.1.1"
-ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+#default_ip="192.168.1.1"
+#ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 # Override default IP if a valid custom IP is provided as the first argument
-[[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
-    echo "Modify default IP address to: ${1}"
-    sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
-}
+#[[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
+#    echo "Modify default IP address to: ${1}"
+#    sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
+#}
 
 # Set the default password for the 'root' user (change empty password to 'password')
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Append source repository information to etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
@@ -31,7 +31,7 @@ echo "DISTRIB_SOURCEBRANCH='v23.05.7'" >>package/base-files/files/etc/openwrt_re
 sed -i '/CONFIG_DEVEL/d' .config
 sed -i '/CONFIG_CCACHE/d' .config
 # Apply new ccache configuration
-if [[ "${2}" == "true" ]]; then
+if [[ "${1}" == "true" ]]; then
     echo "CONFIG_DEVEL=y" >>.config
     echo "CONFIG_CCACHE=y" >>.config
     echo 'CONFIG_CCACHE_DIR="$(TOPDIR)/.ccache"' >>.config
