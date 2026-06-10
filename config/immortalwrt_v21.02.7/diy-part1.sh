07a7315ba934ddca1e5426effd718c9bca1a67a8
Guplax <198092412+guplax@users.noreply.github.com>
2026-06-10 14:30:08 +0000

    Add ImmortalWrt v21.02.7

---
+++ config/immortalwrt_v21.02.7/diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: DIY script (Before updating feeds — modify the default IP, hostname, theme, add/remove packages, etc.)
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: v21.02.7
+#========================================================================================================================
+
+# Add a custom feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Remove unnecessary packages
+# rm -rf package/emortal/{autosamba,ipv6-helper}
+
